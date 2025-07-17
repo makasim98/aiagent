89a8c1f494216ddd069c169512459694a1d774c4
makasim98 <maxluch98@gmail.com>
2025-07-17 14:30:08 -0400

    Set up client for Google gemini and test it

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    cliend = genai.Client(api_key=api_key)
     print("Hello from aiagent!")
+
+    response = cliend.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
